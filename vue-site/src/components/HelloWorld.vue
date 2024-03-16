975b4f10814cf141ba5bd6ea07aa9dc7d7b41486
SteveWyntontje <wyntontjes123@outlook.com>
2024-03-16 14:58:18 +0100

    some tests

--- vue-site/src/components/HelloWorld.vue
+++ vue-site/src/components/HelloWorld.vue
@@ -19,7 +19,11 @@
 
 <script>
 import * as Tone from "./Tone.js"
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
 
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
 export default {
   name: "HelloWorld",
   props: {
