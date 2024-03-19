c53ded61fe6ea76decd4c8f9c931679563f14e5e
SteveWyntontje <wyntontjes123@outlook.com>
2024-03-19 17:27:18 +0100

    Other imports

--- vue-site/src/main.js
+++ vue-site/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Vue from unpkg.com/vue@3.4.21/
+import Vue from 'unpkg.com/vue@3.4.21/'
+import Tone from 'https://unpkg.com/tone@14.8.49/build/Tone.js'
 
 createApp(App).mount('#app')
